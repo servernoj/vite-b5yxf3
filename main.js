58f8f4a5ebabe9b91e04026d08b49e7b8b9f1e20
servernoj <simon.baev@globallogic.com>
2021-08-13 15:28:28 -0700

    1 file modified

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ console.log('Booo');
 
 document.querySelector('#app').innerHTML = `
   <h1>BooHello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">kllk</a>
 `;
